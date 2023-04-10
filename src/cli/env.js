c733f99454a6bc9e9e4020a0947078998cef8f50
xam1dullo <khkhamidullo@gmail.com>
2023-04-10 14:59:21 +0500

    done src/cli tasks

--- src/cli/env.js
+++ src/cli/env.js
@@ -1,5 +1,14 @@
+const prefix = 'RSS_'
 const parseEnv = () => {
-    // Write your code here 
+  
+  const result = Object.entries(process.env)
+    .map(([key, val]) => `${prefix}${key}=${val}`)
+    .join(', ')
+  console.log(result)
+
 };
 
 parseEnv();
+
+
+//USER_ID=12345 USER_NAME=foo node app.js
