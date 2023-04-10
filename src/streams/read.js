891173603bfac031ff03e67987879ac39d7c3f83
Xamidullo Xudoyberdiyev <khkhamidullo@gmail.com>
2023-04-10 16:21:43 +0500

    Update read.js

--- src/streams/read.js
+++ src/streams/read.js
@@ -1,5 +1,19 @@
-const read = async () => {
-    // Write your code here 
+import path from "path";
+import { createReadStream } from "node:fs";
+
+const __dirname = path.dirname(process.argv[1])
+
+const folder = "files";
+const file = "fileToRead.txt";
+
+const read = async (folder, file) => {
+  try {
+    // Write your code here
+    const filePath = path.join(__dirname,folder, file);
+    createReadStream(filePath).pipe(process.stdout);
+  } catch (e) {
+    console.log(e);
+  }
 };
 
-await read();
+await read(folder, file);
