9c629fef96c5009909092983ae891da90ea3b765
Xamidullo Xudoyberdiyev <khkhamidullo@gmail.com>
2023-04-10 16:21:53 +0500

    Update transform.js

--- src/streams/transform.js
+++ src/streams/transform.js
@@ -1,5 +1,14 @@
+import { Transform } from 'stream';
+
+const reverse = new Transform({
+  transform(chunk, encoding, callback) {
+    this.push(chunk.toString().trim().split('').reverse().join(''))
+    callback()
+  }
+})
+
 const transform = async () => {
-    // Write your code here 
+    process.stdin.pipe(reverse).pipe(process.stdout)
 };
 
 await transform();
