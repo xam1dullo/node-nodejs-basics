128ca21e9817cab5c7ec845bfa3d26aa15aa8a7f
xam1dullo <khkhamidullo@gmail.com>
2023-04-10 15:03:08 +0500

    done src/hash  tasks

--- src/hash/calcHash.js
+++ src/hash/calcHash.js
@@ -1,5 +1,26 @@
-const calculateHash = async () => {
-    // Write your code here 
+import crypto from "node:crypto";
+import path from "node:path";
+import fs from "node:fs";
+
+const __dirname = path.dirname(process.argv[1]);
+
+const folder = "files";
+const file = "fileToCalculateHashFor.txt";
+
+const calculateHash = async (folder, file) => {
+  const filePath = path.join(__dirname, folder, file);
+  const hash = await crypto.createHash("sha256");
+
+  const rs = await fs.createReadStream(filePath);
+
+  rs.on("readable", () => {
+    const data = rs.read();
+    if (data) {
+      hash.update(data);
+    } else {
+      console.log(hash.digest("hex"));
+    }
+  });
 };
 
-await calculateHash();
+await calculateHash(folder, file);
