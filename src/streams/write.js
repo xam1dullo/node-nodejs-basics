20249024ae34f5e27f6c4febe96711aa3d50ed8e
xam1dullo <khkhamidullo@gmail.com>
2023-04-10 15:04:39 +0500

    done src/streams  tasks

--- src/streams/write.js
+++ src/streams/write.js
@@ -1,5 +1,20 @@
-const write = async () => {
-    // Write your code here 
+import path from "path";
+import fs from "node:fs";
+
+const __dirname = path.dirname(process.argv[1]);
+
+const folder = "files";
+const file = "fileToRead.txt";
+
+const write = async (folder, file) => {
+  try {
+    // Write your code here
+    const filePath = path.join(__dirname, folder, file);
+    const outputStream = fs.createWriteStream(filePath);
+    process.stdin.pipe(outputStream);
+  } catch (e) {
+    console.log(e);
+  }
 };
 
-await write();
+await write(folder, file);
