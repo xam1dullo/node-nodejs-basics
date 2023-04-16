687ff054ec4bf67c2278eb1e46f9d23f2cb3446e
xam1dullo <khkhamidullo@gmail.com>
2023-04-17 01:37:35 +0500

    update old code add export code

--- src/streams/read.js
+++ src/streams/read.js
@@ -1,19 +1,18 @@
 import path from "path";
 import { createReadStream } from "node:fs";
 
-const __dirname = path.dirname(process.argv[1])
+const __dirname = path.dirname(process.argv[1]);
 
 const folder = "files";
 const file = "fileToRead.txt";
 
-const read = async (folder, file) => {
-  try {
-    // Write your code here
-    const filePath = path.join(__dirname,folder, file);
-    createReadStream(filePath).pipe(process.stdout);
-  } catch (e) {
-    console.log(e);
-  }
+const read = async (filePath) => {
+    try {
+        // Write your code here
+        createReadStream(filePath).pipe(process.stdout);
+    } catch (e) {
+        console.log(e);
+    }
 };
 
-await read(folder, file);
+export default read;
--- src/zip/compress.js
+++ src/zip/compress.js
@@ -8,13 +8,11 @@ const folder = "files";
 const file = "fileToCompress.txt";
 const archiveFileName = "archive.gz";
 
-const compress = async (srcFolder, srcFile, dstnFolder, dstnFile) => {
-  // Write your code here
-  const srcPath = path.join(__dirname, srcFolder, srcFile);
-  const dstnPath = path.join(__dirname, dstnFolder, dstnFile);
-  const zip = createGzip();
-  const output = fs.createWriteStream(dstnPath);
-  await fs.createReadStream(srcPath).pipe(zip).pipe(output);
+const compress = async (sourcePath, destinationPath) => {
+    // Write your code here
+    const zip = createGzip();
+    const output = fs.createWriteStream(destinationPath);
+    fs.createReadStream(sourcePath).pipe(zip).pipe(output);
 };
 
-await compress(folder, file, folder, archiveFileName);
+export default compress;
